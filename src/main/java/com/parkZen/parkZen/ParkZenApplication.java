dcca79d6a1a00681f70d8c7d538d3d9781ca964a
Mohd Aasif <aasifmohd1385@gmail.com>
2026-06-01 12:32:58 +0530

    first commit

---
+++ src/main/java/com/parkZen/parkZen/ParkZenApplication.java
@@ -0,0 +1,13 @@
+package com.parkZen.parkZen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkZenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkZenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkZen/parkZen/ParkZenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkZen.parkZen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkZenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
