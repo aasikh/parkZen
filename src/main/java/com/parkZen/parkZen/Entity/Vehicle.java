e9caac08558a47760cf3d0c79307b26baa4c3f3d
Sairam Gudelliwar <sairamgudelliwar4@gmail.com>
2026-06-06 12:01:27 +0530

    package updated

---
+++ src/main/java/com/parkZen/parkZen/Entity/Vehicle.java
@@ -0,0 +1,55 @@
+package com.parkZen.parkZen.Entity;
+
+import jakarta.persistence.*;
+
+
+    @Entity
+    @Table
+    public class Vehicle {
+
+        private Long id;
+        private String vehicleType;
+        private String vehicleNumber;
+
+        public enum vehicleType{
+            Car,
+            Truck,
+            Bike
+        }
+
+        public Vehicle(){
+        }
+
+        public Vehicle(Long id, String vehicleType, String vehicleNumber){
+            this.id = id;
+            this.vehicleType = vehicleType;
+            this.vehicleNumber = vehicleNumber;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+        public String getVehicleType() {
+            return vehicleType;
+        }
+
+        public void setVehicleType(String vehicleType) {
+            vehicleType = vehicleType;
+        }
+
+        public void getVehicleNumber(String vehicleNumber) {
+            vehicleNumber = vehicleNumber;
+        }
+
+        public String setVehicleNumber() {
+            return vehicleNumber;
+        }
+
+
+    }
+
